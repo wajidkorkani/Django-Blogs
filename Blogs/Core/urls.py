19a262a34f85efbf6ef4c063a452e5dc5df69cae
wajidkorkani <errormrcoder@gmail.com>
2023-10-29 18:39:53 +0500

    Home page has been created with filter option all blogs are listed on home page and users can click on blogs and they will be redirected to the about page of blog where they can read blogs and also base template has been created and all templates will be extended on base template.

--- Blogs/Core/urls.py
+++ Blogs/Core/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path 
+from django.conf import settings
+from django.conf.urls.static import static 
 from Core.views import *
 
 urlpatterns = [
     path('', home, name='home'),
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- Blogs/Core/views.py
+++ Blogs/Core/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Blog
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1> Hello world! </h1>")
+    if 'n-o' in request.GET:
+        blogs = Blog.objects.all().order_by('-time')
+    elif 'o-n'  in request.GET:
+        blogs = Blog.objects.all().order_by('time')
+    else:
+        blogs = Blog.objects.all().order_by('-time')
+    template = 'index.html'
+    context = {'blogs': blogs}
+    return render(request, template, context)
