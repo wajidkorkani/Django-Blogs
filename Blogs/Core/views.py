b47f49d027bf8562e7c65e838960566417123196
wajidkorkani <errormrcoder@gmail.com>
2023-10-29 23:40:26 +0500

    Now the code is clean and comments are added for all views is at top of those.

--- Blogs/Core/views.py
+++ Blogs/Core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Blog
 # Create your views here.
+# This is view for home page. 
 def home(request):
     if 'n-o' in request.GET:
         blogs = Blog.objects.all().order_by('-time')
@@ -9,16 +10,30 @@ def home(request):
     else:
         blogs = Blog.objects.all().order_by('-time')
     template = 'index.html'
-    context = {'blogs': blogs}
+    context = {
+        'blogs': blogs
+        }
     return render(request, template, context)
 
+# This is view for about page 
 def about(request, slug, pk):
     blog =  get_object_or_404(Blog, slug=slug, id=pk)
-    return render (request, 'about.html', {'blog': blog})
+    template = 'about.html'
+    context = {
+        'blog' : blog
+    }
+    return render (request, template, context)
 
+# This is view is for searchbar 
 def searchbar(request):
     searched = request.GET["searched"]
     blog = Blog.objects.filter(title__icontains=searched)
     title  = Blog.objects.filter(title__iexact=searched)
     categoery  = Blog.objects.filter(categoery__iexact=searched)
-    return render(request, 'searchbar.html', {'blog': blog, 'blog_title': title, 'blog_categoery': categoery})
+    template = 'searchbar.html'
+    context = {
+        'blog': blog, 
+        'blog_title': title, 
+        'blog_categoery': categoery
+        }
+    return render(request, template, context)
